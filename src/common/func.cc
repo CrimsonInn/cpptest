4c88814df810acf69da7bd9cd413089e31230063
Qin Lyu <qlyu@g.harvard.edu>
2017-04-01 18:14:22 -0400

    initilization

---
+++ src/common/func.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print() {
+    std::cout << "printed from func.cc" << std::endl;
+}
---
+++ src/main.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include "./func.h"
+#include "./func2.h"
+
+using namespace std;
+void calc(float *A, size_t n) {
+    float tmp = 0.0;
+    for (size_t i = 0;i < n; ++i)
+        tmp += A[i];
+}
+
+
+int main()
+{
+    print();
+    print2();
+    vector<thread> th;
+
+    float *A = new float[1<<28];
+    size_t num_threads = 3;
+    for (size_t i = 0;i < num_threads; ++i)
+        th.push_back(thread(calc, A, 1<<28));
+    for (auto &t : th) {
+        t.join();
+    }
+    cout << thread::hardware_concurrency() << endl;
+    return 0;
+}
+
